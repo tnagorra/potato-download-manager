f0f3c38c0bf07a65bfb3a4f916a7734f08d49c0c
tnagorra <weathermist@gmail.com>
2014-11-28 17:51:06 +0545

    Added helper functions in common/helper.cpp

---
+++ include/common/helper.h
@@ -0,0 +1,38 @@
+// File helper.h
+// Contains helper functions
+
+#ifndef __CO_HELPER__
+#define __CO_HELPER__
+#include <string>
+#include <sstream>
+#include <iostream>
+#include <iomanip>
+#include <climits>
+#include <cmath>
+#include <ctime>
+
+// Infinity ∞ unicode
+
+// Returns the time in formatted form
+std::string formatTime(uintmax_t time) ;
+
+// Returns the bytes in formatted form
+std::string formatByte(uintmax_t byte);
+
+// Retuns a random alphanumeric string
+std::string randomString(uintmax_t len=32);
+
+// Returns a number rounded to n digits after decimal points
+inline double round(double num,uintmax_t n){
+    unsigned tmp = pow(10,2);
+    return std::round(num*tmp)/tmp;
+}
+
+// Helper to sort filenames according to numerical value
+/*
+   inline bool numerically(std::string const & a, std::string const & b){
+   return stoi(a) < stoi(b);
+   }
+*/
+
+#endif
--- src/filesystem.cpp
+++ src/filesystem.cpp
@@ -2,6 +2,7 @@
 #include<string>
 #include"filesystem/Directory.h"
 #include"filesystem/File.h"
+#include"common/helper.h"
 
 int main() try {
     File b(".temporary");
@@ -11,7 +12,17 @@ int main() try {
     if (b.exists()){
         b.move("ok",Node::FORCE);
         std::cout << b.size() << std::endl;
+        b.remove();
     }
+
+    /*
+    // Some Helper functions
+    std::cout << formatTime(23823992)<<std::endl;
+    std::cout << formatByte(1024*1024+343234)<<std::endl;
+    std::cout << round(1243.234222343,3) << std::endl;
+    std::cout << randomString()<<std::endl;
+    */
+
  }
  catch (fs::filesystem_error& e){
     std::cout << e.what() << std::endl;
